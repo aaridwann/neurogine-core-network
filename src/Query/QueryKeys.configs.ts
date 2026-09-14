ca9950f86bcfd12542e044e8506a5a9a5d122ff6
aaridwann <aaridwann@gmail.com>
2026-09-14 13:29:43 +0700

    feat(product-detail): [neurogine-assessment] add unit test and adjust export file

--- src/Query/QueryKeys.configs.ts
+++ src/Query/QueryKeys.configs.ts
@@ -9,8 +9,7 @@ export const createQueryKeys = <T extends string>(
   return {
     all: [scope] as const,
     lists: () => [scope, 'list'] as const,
-    list: (filters: Record<string, unknown>) =>
-            [scope, 'list', filters] as const,
+    list: (filters: Record<string, unknown>) => [scope, 'list', filters] as const,
     details: () => [scope, 'detail'] as const,
     detail: (id: string | number) => [scope, 'detail', id] as const,
   };
